a43aab6b9d10f46cb1b564d71ec25b5224386a3f
aditya0249 <aditya0249.becse24@chitkara.edu.in>
2025-02-05 10:42:55 -0800

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,23 +4,9 @@ int main() {
     int n1,n2,result;
     char c;
     scanf("%d %d %c",&n1,&n2,&c);
-    switch(c){
-        case '+':
+    if(c=='+'){
         result=n1+n2;
         printf("%d",result);
-        break;
-        case '-':
-        result=n1-n2;
-        printf("%d",result);
-        break;
-        case '*':
-        result=n1*n2;
-        printf("%d",result);
-        break;
-        case '/':
-        result=n1/n2;
-        printf("%d",result);
-        
     }
     return 0;
 }
