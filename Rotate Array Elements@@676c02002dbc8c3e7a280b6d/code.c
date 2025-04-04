1a788e4f6406b3d4f409ab889847cd659247096a
aditya0249 <aditya0249.becse24@chitkara.edu.in>
2025-04-04 12:47:23 -0700

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
+
 int main(){
     int num,k;
-    int arr[num];
     scanf("%d",&num);
+    int arr[num];
     for(int i=0; i<num; i++){
         scanf("%d",&arr[i]);
     }
     scanf("%d",&k);
-    int temp=num+k;
-    arr[temp];
-    int n_num=num-1;
-    int t_temp=temp-1;
-    for(int j=t_temp; j>=k; j--){
-        arr[j]=arr[n_num];
-        n_num--;
-
+    int n_arr[k];
+    arr[num+k];
+    int temp=num-k;
+    for(int i=0; i<k; i++){
+        n_arr[i]=arr[temp];
+        temp++;
     }
-    int diff=num-k;
-    int n_diff=diff+k;
-    for(int l=0; l>k; l++){
-        arr[0]=arr[n_diff];
-        n_diff++;
+    int count=num-(k+1);
+    for(int i=0; i<k; i++){
+        for(int j=count; j>=0; j--){
+            arr[j+1]=arr[j];
+        }
+        count++;
     }
-    arr[num-k];
-    for(int y=0; y<temp; y++){
-        printf("%d\n",arr[y]);
+    for(int i=0; i<k; i++){
+        arr[i]=n_arr[i];
     }
     
+    for(int k=0; k<num; k++){
+        printf("%d ",arr[k]);
 
+    }
 }
