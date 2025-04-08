e7dfed047c22f88d20205ca1f42c8533af537c40
aditya0249 <aditya0249.becse24@chitkara.edu.in>
2025-04-08 10:15:57 -0700

    Solution to Find the Maximum and Minimum Element in an Array

--- Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n,temp=0;
-    scanf("%d",&n);
+int main() {
+    int n, temp = 0;
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
-
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
     
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(arr[j+1]>=arr[j]){
-            continue;
+    // Bubble Sort
+    for (int i = 0; i < n - 1; i++) {  // Outer loop should go up to n-1
+        for (int j = 0; j < n - i - 1; j++) {  // Inner loop should go up to n-i-1
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
     }
-        temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-        
 
-    }
-    printf("%d %d\n",arr[0],arr[n-1]);
+    // Output the first and last element of the sorted array
+    printf("%d %d\n", arr[0], arr[n-1]);
 }
